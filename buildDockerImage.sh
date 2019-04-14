5364f5a94dc246f6e99a65ecd1429b05880022e7
Josef <josef.grieb@gmail.com>
2019-04-14 21:55:39 +0200

    Travis Setup

---
+++ buildDockerImage.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#!/bin/bash
+
+docker build -t anyvr-lemon:$TRAVIS_BUILD_ID .
+docker tag anyvr-lemon:$TRAVIS_BUILD_ID $AWS_DOCKER_REGISTRY/anyvr-lemon:$TRAVIS_BUILD_ID
+docker push $AWS_DOCKER_REGISTRY/anyvr-lemon:$TRAVIS_BUILD_ID
+
+ssh -i ~/.ssh/travis_key anyvr@159.89.19.236 "bash -s" < deploy.sh "$AWS_DOCKER_REGISTRY/anyvr-lemon:$TRAVIS_BUILD_ID"
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#!/bin/bash
+if [ "$(docker ps -aq -f name=anyvr)" ]; then
+        if [ "$(docker ps -q -f name=anyvr)" ]; then  # check if docker container currently running
+                docker stop anyvr-lemon
+        fi
+        docker rm anyvr
+        docker run -d --name anyvr-lemon -p 7000:7000/udp -v /home/anyvr/logs/anyvr-lemon:/app/logs $1
+else
+        docker run -d --name anyvr-lemon -p 7000:7000/udp -v /home/anyvr/logs/anyvr-lemon:/app/logs $1
+fi
+
+
+
