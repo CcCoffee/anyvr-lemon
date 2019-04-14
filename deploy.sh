3d8107aa3e423be836d27ec6049119c0e099d347
Josef <josef.grieb@gmail.com>
2019-04-14 23:01:08 +0200

    change container anyvr-lemon

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ "$(docker ps -aq -f name=anyvr-lemon)" ]; then
         if [ "$(docker ps -q -f name=anyvr-lemon)" ]; then  # check if docker container currently running
                 docker stop anyvr-lemon
         fi
-        docker rm anyvr
+        docker rm anyvr-lemon
         docker run -d --name anyvr-lemon -p 7000:7000/udp -v /home/anyvr/logs/anyvr-lemon:/app/logs $1
 else
         docker run -d --name anyvr-lemon -p 7000:7000/udp -v /home/anyvr/logs/anyvr-lemon:/app/logs $1
