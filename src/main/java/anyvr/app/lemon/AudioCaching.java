06d24d74fcc51f2ac4693315563a31bbf7018327
Josef <josef.grieb@gmail.com>
2019-04-19 01:00:47 +0200

    Write audio file

--- src/main/java/anyvr/app/lemon/AudioCaching.java
+++ src/main/java/anyvr/app/lemon/AudioCaching.java
@@ -1,28 +1,27 @@
 package anyvr.app.lemon;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class AudioCaching {
 
-    private List<List> audioVoices;
+    private final Object lockAudioVoice = new Object();
+    private AudioVoice audioVoices;
 
     public AudioCaching() {
-        audioVoices = new ArrayList<>();
-        audioVoices = Collections.synchronizedList(audioVoices);
+        //audioVoices = new ArrayList<>();
+        //audioVoices = Collections.synchronizedList(audioVoices);
     }
 
     public void findPlayerWithSameTimestamp(AudioVoice currentAudioVoice) {
 
-        audioVoices.stream().filter(voice -> {
-            AudioVoice oldAudioVoice2 = (AudioVoice) voice;
-            if((oldAudioVoice2.getTimestemps() + 20) < currentAudioVoice.getTimestemps()) {
-                return true;
-            } else {
-                return false;
-            }
-        }).findFirst();
+//        audioVoices.stream().filter(voice -> {
+//            AudioVoice oldAudioVoice2 = (AudioVoice) voice;
+//            if((oldAudioVoice2.getTimestemps() + 20) < currentAudioVoice.getTimestemps()) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        }).findFirst();
 
         //override write on Disk
     }
--- src/main/java/anyvr/app/lemon/handler/VoiceHandler.java
+++ src/main/java/anyvr/app/lemon/handler/VoiceHandler.java
@@ -1,12 +1,17 @@
 package anyvr.app.lemon.handler;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import anyvr.Spec;
+import anyvr.app.lemon.jni.Opus;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -16,16 +21,26 @@ import io.netty.util.concurrent.GlobalEventExecutor;
 
 public class VoiceHandler extends SimpleChannelInboundHandler<Spec.PlayerVoice> {
 
+    private static final String outPutFileName = "/app/logs/javaUnityDecoded.wav";
+    //private static final String encodedFileName = "/Users/josef/Development/C_Language/javaUnityEncoded.wav";
+    private OutputStream outputStream;
+    //rivate OutputStream encodedStream;
+
+    private static final int MAX_PACKET_SIZE = 4 * 1276;
+    private static final int MAX_FRAME_SIZE = 6 * 480;
+    private static final int CHANNELS = 1;
+
     private static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
     private static Logger logger = LogManager.getLogger(VoiceHandler.class);
     private long opusDecoder;
 
-    public VoiceHandler(final long opusDecoder) {
+    public VoiceHandler(final long opusDecoder) throws IOException {
         this.opusDecoder = opusDecoder;
-    }
 
-    public VoiceHandler() {
-
+        Path fileOutput = Paths.get(outPutFileName);
+        //Path fileEncoded = Paths.get(encodedFileName);
+        outputStream = Files.newOutputStream(fileOutput);
+        //encodedStream = Files.newOutputStream(fileEncoded);
     }
 
     @Override
@@ -45,6 +60,13 @@ public class VoiceHandler extends SimpleChannelInboundHandler<Spec.PlayerVoice> 
 
         logger.info("UUID: " + playerVoice.getUuid());
         logger.info("Audio: " + Arrays.toString(playerVoice.getVoice().toByteArray()));
+
+        byte[] output = new byte[MAX_FRAME_SIZE * CHANNELS * 2];
+
+        int currentFrameSize = Opus
+                .decode(opusDecoder, playerVoice.getVoice().toByteArray(), 0, playerVoice.getVoice().toByteArray().length, output, 0, MAX_FRAME_SIZE, 0);
+
+        outputStream.write(output, 0, currentFrameSize * CHANNELS * 2);
 
         for (Channel channel : channels) {
             if (ctx.channel() != channel) {
