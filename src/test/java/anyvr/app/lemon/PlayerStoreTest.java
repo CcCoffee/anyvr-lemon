98f30a445b93d2c50e2ffb33383bcdda66808609
Josef <josef.grieb@gmail.com>
2019-04-22 20:00:46 +0200

    disable epoll

---
+++ src/test/java/anyvr/app/lemon/PlayerStoreTest.java
@@ -0,0 +1,13 @@
+package anyvr.app.lemon;
+
+import org.junit.Test;
+
+public class PlayerStoreTest {
+
+    @Test
+    public void playerStoreTest() {
+        PlayerStore playerStore = new PlayerStore();
+
+
+    }
+}
