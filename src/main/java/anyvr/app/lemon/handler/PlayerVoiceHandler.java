f595c52e8a542b9ab603e5d898f29992fdd058a0
Josef <josef.grieb@gmail.com>
2019-04-25 07:30:05 +0200

    clean code

--- src/main/java/anyvr/app/lemon/handler/PlayerVoiceHandler.java
+++ src/main/java/anyvr/app/lemon/handler/PlayerVoiceHandler.java
@@ -1,20 +1,12 @@
 package anyvr.app.lemon.handler;
 
-import static java.util.UUID.fromString;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,33 +15,19 @@ import anyvr.Spec;
 import anyvr.app.lemon.Player;
 import anyvr.app.lemon.PlayerStore;
 import anyvr.app.lemon.jni.Opus;
-import com.google.protobuf.ByteString;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.channel.group.ChannelGroup;
-import io.netty.channel.group.DefaultChannelGroup;
-import io.netty.util.concurrent.GlobalEventExecutor;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-public class VoiceHandler extends SimpleChannelInboundHandler<Spec.PlayerVoice> {
+public class PlayerVoiceHandler extends SimpleChannelInboundHandler<Spec.PlayerVoice> {
 
-    private String outPutFileName = "/app/logs/";
-    private static final int MAX_PACKET_SIZE = 4 * 1276;
+    private final String outPutFileName = "/app";
     private static final int MAX_FRAME_SIZE = 6 * 480;
     private static final int CHANNELS = 1;
-    private static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
     private static final int SAMPLE_RATE = 24000;
-    private static Logger logger = LogManager.getLogger(VoiceHandler.class);
-
+    private static Logger logger = LogManager.getLogger(PlayerVoiceHandler.class);
     private PlayerStore playerStore = new PlayerStore();
-
-    @Override
-    public void channelActive(final ChannelHandlerContext ctx) throws Exception {
-        //        Channel incoming = ctx.channel();
-        //        channels.add(incoming);
-    }
+    private volatile int lastDatagramId;
+    private final Object lockDatagramId = new Object();
 
     @Override
     public void channelInactive(final ChannelHandlerContext ctx) throws Exception {
@@ -58,14 +36,7 @@ public class VoiceHandler extends SimpleChannelInboundHandler<Spec.PlayerVoice> 
 
     @Override
     protected void channelRead0(final ChannelHandlerContext ctx, final Spec.PlayerVoice playerVoice) throws Exception {
-
-        if (playerVoice == null) {
-            throw new NullPointerException("Player Voice is null");
-        }
-
-        if (playerVoice.getUuid() == null) {
-            throw new NullPointerException("Uuid is null");
-        }
+        IsUdpIsInSequence(playerVoice.getDatagramOrderId());
 
         logger.info("UUID: " + playerVoice.getUuid());
         logger.info("Audio: " + Arrays.toString(playerVoice.getVoice().toByteArray()));
@@ -81,7 +52,6 @@ public class VoiceHandler extends SimpleChannelInboundHandler<Spec.PlayerVoice> 
             OutputStream audioFile = Files.newOutputStream(fileOutput);
 
             if (decoder == 0) {
-                //throw Exception
                 logger.error("Creating Decoder Error");
             }
 
@@ -98,10 +68,20 @@ public class VoiceHandler extends SimpleChannelInboundHandler<Spec.PlayerVoice> 
                     currentPlayer.getChannel().writeAndFlush(playerVoice);
                 });
 
-        writeFile(player, playerVoice);
+        checkIfHaveToFillTheGap(player, playerVoice);
     }
 
-    private void writeFile(Player player, Spec.PlayerVoice playerVoice) throws IOException {
+    private boolean IsUdpIsInSequence(int currentDatagrammId) {
+        synchronized (lockDatagramId) {
+            if (currentDatagrammId > lastDatagramId) {
+                lastDatagramId = currentDatagrammId;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private void checkIfHaveToFillTheGap(Player player, Spec.PlayerVoice playerVoice) throws IOException {
 
         synchronized (player.getLock()) {
             byte[] output = new byte[MAX_FRAME_SIZE * CHANNELS * 2];
@@ -110,30 +90,26 @@ public class VoiceHandler extends SimpleChannelInboundHandler<Spec.PlayerVoice> 
                     .decode(player.getAudioDecoder(), playerVoice.getVoice().toByteArray(), 0, playerVoice.getVoice().toByteArray().length, output, 0,
                             MAX_FRAME_SIZE, 0);
 
-            if (player.getLastTimestamp() == 0) {
-                byte[] audioStream = new byte[currentFrameSize * CHANNELS * 2];
-                for (int i = 0; i < currentFrameSize * CHANNELS * 2; i++) {
-                    audioStream[i] = output[i];
-                }
-                player.getAudioFile().write(audioStream);
+            if ((player.getLastTimestamp() == 0) || (player.getLastTimestamp() + 20) == playerVoice.getTimestamp()) {
+                writePlayerAudioFile(currentFrameSize, output, player);
             } else {
-                if (player.getLastTimestamp() + 20 == playerVoice.getTimestamp()) {
-                    byte[] audioStream = new byte[currentFrameSize * CHANNELS * 2];
-                    for (int i = 0; i < currentFrameSize * CHANNELS * 2; i++) {
-                        audioStream[i] = output[i];
-                    }
-                    player.getAudioFile().write(audioStream);
-                } else {
-                    int fillGapCounter = (int) ((playerVoice.getTimestamp() - player.getLastTimestamp()) / 20);
+                int fillGapCounter = (int) ((playerVoice.getTimestamp() - player.getLastTimestamp()) / 20);
 
-                    for (int i = 0; i < fillGapCounter; i++) {
-                        byte[] audioStream = new byte[currentFrameSize * CHANNELS * 2];
-                        player.getAudioFile().write(audioStream);
-                    }
+                for (int i = 0; i < fillGapCounter; i++) {
+                    byte[] audioStream = new byte[currentFrameSize * CHANNELS * 2];
+                    player.getAudioFile().write(audioStream);
                 }
             }
             player.setLastTimestamp(playerVoice.getTimestamp());
         }
+    }
+
+    private void writePlayerAudioFile(int currentFrameSize, byte[] output, Player player) throws IOException {
+        byte[] audioStream = new byte[currentFrameSize * CHANNELS * 2];
+        for (int i = 0; i < currentFrameSize * CHANNELS * 2; i++) {
+            audioStream[i] = output[i];
+        }
+        player.getAudioFile().write(audioStream);
     }
 
     @Override
--- src/main/java/anyvr/app/lemon/jni/Opus.java
+++ src/main/java/anyvr/app/lemon/jni/Opus.java
@@ -32,303 +32,60 @@ public class Opus {
     public static native int decode(long decoder, byte[] input, int inputOffset, int inputLength, byte[] output,
             int outputOffset, int outputFrameSize, int decodeFEC);
 
-    /**
-     * Creates an OpusDecoder structure, returns a pointer to it or 0 on error.
-     *
-     * @param Fs       Sample rate to decode to
-     * @param channels number of channels to decode to(1/2)
-     *
-     * @return A pointer to the OpusDecoder structure created, 0 on error.
-     */
     public static native long decoder_create(int Fs, int channels);
 
-    /**
-     * Destroys an OpusDecoder, freeing it's resources.
-     *
-     * @param decoder Address of the structure (as returned from decoder_create)
-     */
     public static native void decoder_destroy(long decoder);
 
-    /**
-     * Returns the number of samples in an opus packet
-     *
-     * @param decoder The decoder to use.
-     * @param packet  Array holding the packet.
-     * @param offset  Offset into packet where the actual packet begins.
-     * @param length  Length of the packet.
-     *
-     * @return the number of samples in <tt>packet</tt> .
-     */
     public static native int decoder_get_nb_samples(long decoder, byte[] packet, int offset, int length);
 
-    /**
-     * Returns the size in bytes required for an OpusDecoder structure.
-     *
-     * @param channels number of channels (1/2)
-     *
-     * @return the size in bytes required for an OpusDecoder structure.
-     */
     public static native int decoder_get_size(int channels);
 
-    /**
-     * Encodes the input from <tt>input</tt> into an opus packet in <tt>output</tt>.
-     *
-     * @param encoder        The encoder to use.
-     * @param input          Array containing PCM encoded input.
-     * @param inputOffset    Offset to use into the <tt>input</tt> array
-     * @param inputFrameSize The number of samples per channel in <tt>input</tt>.
-     * @param output         Array where the encoded packet will be stored.
-     * @param outputOffset
-     * @param outputLength   The number of available bytes in <tt>output</tt>.
-     *
-     * @return The number of bytes written in <tt>output</tt>, or a negative on
-     *         error.
-     */
     public static native int encode(long encoder, byte[] input, int inputOffset, int inputFrameSize, byte[] output,
             int outputOffset, int outputLength);
 
-    /**
-     * Creates an OpusEncoder structure, returns a pointer to it casted to long. The
-     * native function's <tt>application</tt> parameter is always set to
-     * OPUS_APPLICATION_VOIP.
-     *
-     * @param Fs       Sample rate of the input PCM
-     * @param channels number of channels in the input (1/2)
-     *
-     * @return A pointer to the OpusEncoder structure created, 0 on error
-     */
     public static native long encoder_create(int Fs, int channels);
 
-    /**
-     * Destroys an OpusEncoder, freeing it's resources.
-     *
-     * @param encoder Address of the structure (as returned from encoder_create)
-     */
     public static native void encoder_destroy(long encoder);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Returns the
-     * current encoder audio bandwidth .
-     *
-     * @param encoder The encoder to use
-     *
-     * @return the current encoder audio bandwidth
-     */
     public static native int encoder_get_bandwidth(long encoder);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Returns the
-     * current encoder bitrate.
-     *
-     * @param encoder The encoder to use
-     *
-     * @return The current encoder bitrate.
-     */
     public static native int encoder_get_bitrate(long encoder);
 
     public static native int encoder_get_complexity(long encoder);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Returns the
-     * current DTX setting of the encoder.
-     *
-     * @param encoder The encoder to use
-     *
-     * @return the current DTX setting of the encoder.
-     */
     public static native int encoder_get_dtx(long encoder);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Returns the
-     * current inband FEC encoder setting.
-     *
-     * @param encoder The encoder to use
-     *
-     * @return the current inband FEC encoder setting.
-     */
     public static native int encoder_get_inband_fec(long encoder);
 
-    /**
-     * Returns the size in bytes required for an OpusEncoder structure.
-     *
-     * @param channels number of channels (1/2)
-     *
-     * @return the size in bytes required for an OpusEncoder structure.
-     */
     public static native int encoder_get_size(int channels);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Returns the
-     * current encoder VBR setting
-     *
-     * @param encoder The encoder to use
-     *
-     * @return The current encoder VBR setting.
-     */
     public static native int encoder_get_vbr(long encoder);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Returns the
-     * current VBR constraint encoder setting.
-     *
-     * @param encoder The encoder to use
-     *
-     * @return the current VBR constraint encoder setting.
-     */
     public static native int encoder_get_vbr_constraint(long encoder);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the
-     * encoder audio bandwidth.
-     *
-     * @param encoder   The encoder to use
-     * @param bandwidth The bandwidth to set, should be one of
-     *                  <tt>BANDWIDTH_FULLBAND</tt>, <tt>BANDWIDTH_MEDIUMBAND</tt>,
-     *                  <tt>BANDWIDTH_NARROWBAND</tt>,
-     *                  <tt>BANDWIDTH_SUPERWIDEBAND</tt> or
-     *                  <tt>BANDWIDTH_WIDEBAND</tt>.
-     *
-     * @return OPUS_OK on success
-     */
     public static native int encoder_set_bandwidth(long encoder, int bandwidth);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the
-     * encoder bitrate
-     *
-     * @param encoder The encoder to use
-     * @param bitrate The bitrate to set
-     *
-     * @return <tt>OPUS_OK</tt> on success
-     */
     public static native int encoder_set_bitrate(long encoder, int bitrate);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the
-     * encoder complexity setting.
-     *
-     * @param encoder    The encoder to use
-     * @param complexity The complexity level, from 1 to 10
-     *
-     * @return OPUS_OK on success
-     */
     public static native int encoder_set_complexity(long encoder, int complexity);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the DTX
-     * setting of the encoder.
-     *
-     * @param encoder The encoder to use
-     * @param dtx     0 to turn DTX off, non-zero to turn it on
-     *
-     * @return OPUS_OK on success
-     */
     public static native int encoder_set_dtx(long encoder, int dtx);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the force
-     * channels setting of the encoder.
-     *
-     * @param encoder       The encoder to use
-     * @param forcechannels Number of channels
-     *
-     * @return OPUS_OK on success
-     */
     public static native int encoder_set_force_channels(long encoder, int forcechannels);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the
-     * encoder FEC setting.
-     *
-     * @param encoder   The encoder to use
-     * @param inbandFEC 0 to turn FEC off, non-zero to turn it on.
-     *
-     * @return OPUS_OK on success
-     */
     public static native int encoder_set_inband_fec(long encoder, int inbandFEC);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the
-     * maximum audio bandwidth to be used by the encoder.
-     *
-     * @param encoder      The encoder to use
-     * @param maxBandwidth The maximum bandwidth to use, should be one of
-     *                     <tt>BANDWIDTH_FULLBAND</tt>,
-     *                     <tt>BANDWIDTH_MEDIUMBAND</tt>,
-     *                     <tt>BANDWIDTH_NARROWBAND</tt>,
-     *                     <tt>BANDWIDTH_SUPERWIDEBAND</tt> or
-     *                     <tt>BANDWIDTH_WIDEBAND</tt>
-     *
-     * @return <tt>OPUS_OK</tt> on success.
-     */
     public static native int encoder_set_max_bandwidth(long encoder, int maxBandwidth);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the
-     * encoder's expected packet loss percentage.
-     *
-     * @param encoder        The encoder to use
-     * @param packetLossPerc
-     *
-     * @return OPUS_OK on success.
-     */
     public static native int encoder_set_packet_loss_perc(long encoder, int packetLossPerc);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the
-     * encoder VBR setting
-     *
-     * @param encoder The encoder to use
-     * @param vbr     0 to turn VBR off, non-zero to turn it on.
-     *
-     * @return OPUS_OK on success
-     */
     public static native int encoder_set_vbr(long encoder, int vbr);
 
-    /**
-     * Wrapper around the native <tt>opus_encoder_ctl</tt> function. Sets the
-     * encoder VBR constraint setting
-     *
-     * @param encoder  The encoder to use
-     * @param use_cvbr 0 to turn VBR constraint off, non-zero to turn it on.
-     *
-     * @return OPUS_OK on success
-     */
     public static native int encoder_set_vbr_constraint(long encoder, int use_cvbr);
 
-    /**
-     * Returns the audio bandwidth of an Opus packet, one of
-     * <tt>BANDWIDTH_FULLBAND</tt>, <tt>BANDWIDTH_MEDIUMBAND</tt>,
-     * <tt>BANDWIDTH_NARROWBAND</tt>, <tt>BANDWIDTH_SUPERWIDEBAND</tt> or
-     * <tt>BANDWIDTH_WIDEBAND</tt>, or <tt>INVALID_PACKET</tt> on error.
-     *
-     * @param data   Array holding the packet.
-     * @param offset Offset into packet where the actual packet begins.
-     *
-     * @return one of <tt>BANDWIDTH_FULLBAND</tt>, <tt>BANDWIDTH_MEDIUMBAND</tt>,
-     *         <tt>BANDWIDTH_NARROWBAND</tt>, <tt>BANDWIDTH_SUPERWIDEBAND</tt>,
-     *         <tt>BANDWIDTH_WIDEBAND</tt>, or <tt>INVALID_PACKET</tt> on error.
-     */
     public static native int packet_get_bandwidth(byte[] data, int offset);
 
-    /**
-     * Returns the number of channels encoded in an Opus packet.
-     *
-     * @param data   Array holding the packet.
-     * @param offset Offset into packet where the actual packet begins.
-     *
-     * @return the number of channels encoded in <tt>data</tt>.
-     */
     public static native int packet_get_nb_channels(byte[] data, int offset);
 
-    /**
-     * Returns the number of frames in an Opus packet.
-     *
-     * @param packet Array holding the packet.
-     * @param offset Offset into packet where the actual packet begins.
-     * @param length Length of the packet.
-     *
-     * @return the number of frames in <tt>packet</tt>.
-     */
     public static native int packet_get_nb_frames(byte[] packet, int offset, int length);
 }
